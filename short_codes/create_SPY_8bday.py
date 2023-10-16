a3acb8b5a95b7654522c3ec2041cb6577ed3c0c6
unknown <longhao.zhuo@gmail.com>
2023-10-16 14:09:33 -0400

    code to convert daily data to the first x business day of each month

---
+++ short_codes/create_SPY_8bday.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+spy = pd.read_csv('SPY.csv')
+spy['Date2'] = pd.to_datetime(spy['Date'])
+spy.set_index('Date2', inplace=True)
+
+Begin_of_Month = pd.bdate_range('1993-01-29', '2023-10-13', freq='BMS') + pd.offsets.BusinessDay(5)
+try:
+    spy = spy.loc[Begin_of_Month]
+except Exception as e:
+    print(e)
+    spy = spy.loc[spy.index.isin(Begin_of_Month)]
+
+spy.to_csv('SPY_xbday.csv', index=False)
